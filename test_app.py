bef7136e63af1dcdf041cf239f00773b308a43e5
Jinmun-Park <pjm9827@gmail.com>
2021-10-10 12:06:11 +0800

    check

--- test_app.py
+++ test_app.py
@@ -4,7 +4,6 @@ from googleapiclient.discovery import build #GOOGLE API
 from pandas import json_normalize
 # YAML READER SETUP
 import os
-from datetime import datetime
 # PICKLE SETUP
 import pickle
 # LOG SETUP
@@ -12,7 +11,8 @@ import pandas as pd
 from sqlalchemy import create_engine
 from datetime import datetime, date
 import calendar
-from boto.s3.connection import S3Connection
+# PSYCOG2
+import psycopg2
 
 def read_pickle(file_name: str) -> pd.DataFrame:
     return pd.read_pickle('Pickle/' + file_name)
@@ -46,13 +46,13 @@ def api_youtube_popular(name, max_result):
     pd.options.mode.chained_assignment = None  # Off warning messages, default='warn'
     starttime = datetime.now()
     print(starttime)
-    dictionary = {0: 'Wiki_Category_1', 1: 'Wiki_Category_2', 2: 'Wiki_Category_3', 3: 'Wiki_Category_4',
-                  4: 'Wiki_Category_5', 5: 'Wiki_Category_6'}
+    dictionary = {0: 'wiki_category_1', 1: 'wiki_category_2', 2: 'wiki_category_3', 3: 'wiki_category_4',
+                  4: 'wiki_category_5', 5: 'wiki_category_6'}
     dictionary_list = list(dictionary.values())
     pickle_name = name
 
     # ====================== Retrieving API and store in DF  ======================#
-    service_key = S3Connection(os.environ['YOUTUBE_API_KEY'])
+    service_key = os.environ['YOUTUBE_API_KEY']
     youtube = build('youtube', 'v3', developerKey=service_key)
 
     try:
@@ -83,28 +83,29 @@ def api_youtube_popular(name, max_result):
          'status.madeForKids']]
 
     # Rename Columns
-    df_popular.rename(columns={'snippet.title': 'VideoTitle',
-                               'id': 'VideoId',
-                               'snippet.channelTitle': 'ChannelTitle',
-                               'snippet.channelId': 'ChannelId',
-                               'snippet.publishedAt': 'PublishedAt',
-                               'snippet.tags': 'Tags',
-                               'snippet.categoryId': 'CategoryId',
-                               'statistics.viewCount': 'ViewCount',
-                               'statistics.likeCount': 'LikeCount',
-                               'statistics.dislikeCount': 'DislikeCount',
-                               'statistics.favoriteCount': 'FavoriteCount',
-                               'topicDetails.topicCategories': 'TopicCategories',
-                               'status.madeForKids': 'ForKids',
+    df_popular.rename(columns={'snippet.title': 'video_title',
+                               'id': 'video_id',
+                               'snippet.channelTitle': 'channel_title',
+                               'snippet.channelId': 'channel_id',
+                               'snippet.publishedAt': 'published_at',
+                               'snippet.tags': 'tags',
+                               'snippet.categoryId': 'category_id',
+                               'statistics.viewCount': 'view_count',
+                               'statistics.likeCount': 'like_count',
+                               'statistics.dislikeCount': 'dislike_count',
+                               'statistics.favoriteCount': 'favorite_count',
+                               'statistics.commentCount': 'comment_count',
+                               'topicDetails.topicCategories': 'topic_categories',
+                               'status.madeForKids': 'for_kids',
                                }, inplace=True)
 
     # Reset Index
     df_popular = df_popular.reset_index(drop=True)
 
     # Split TopicCategories URL
-    catrgory_split = df_popular['TopicCategories']
+    catrgory_split = df_popular['topic_categories']
     catrgory_split = pd.DataFrame(catrgory_split)
-    catrgory_split = catrgory_split['TopicCategories'].apply(pd.Series).rename(columns=dictionary)
+    catrgory_split = catrgory_split['topic_categories'].apply(pd.Series).rename(columns=dictionary)
 
     # Filter columns based on the length
     dictionary_list = dictionary_list[0:len(catrgory_split.columns)]
@@ -121,18 +122,18 @@ def api_youtube_popular(name, max_result):
 
     # Merge & Rename columns
     df_popular = df_popular.merge(catrgory_split, left_index=True, right_index=True)
-    del df_popular['TopicCategories']
+    del df_popular['topic_categories']
     print('Youtube Video List : Data mapping has been successfully completed')
 
     # ====================== YOUTUBE_VIDEO_CATEGORY : Data Mapping  ======================#
     df_videocategory = df_videocategory[['id', 'snippet.title']]
-    df_videocategory.rename(columns={'id': 'CategoryId',
-                                     'snippet.title': 'Reg_Category'
+    df_videocategory.rename(columns={'id': 'category_id',
+                                     'snippet.title': 'reg_category'
                                      }, inplace=True)
     print('Youtube Video Category : Data mapping has been successfully completed')
 
     # ====================== MERGE : df_popular & df_videocategory ====================== #
-    df_popular = df_popular.merge(df_videocategory, how='inner', on='CategoryId')
+    df_popular = df_popular.merge(df_videocategory, how='inner', on='category_id')
 
     # ====================== Export to Pickle & Read  ======================#
     picke_replace(name=pickle_name, file=df_popular)
@@ -145,8 +146,56 @@ def api_youtube_popular(name, max_result):
     print('Time taken : ' + timetaken.__str__())
 
     return youtube_popular
-  
-def chart_export(key):
+
+def connect(params_dic):
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params_dic)
+        print("Connection successful")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    return conn
+
+def config(host, database, user, port, password):
+
+    params_dic = {
+        "host": host,
+        "database": database,
+        "port": port,
+        "user":user,
+        "password": password
+    }
+    return params_dic
+
+def insert_table(curr, run_date, day, video_title, video_id, channel_title, channel_id, published_at, tags, category_id, view_count, like_count, dislike_count, favorite_count, comment_count, for_kids, wiki_category_1, wiki_category_2, wiki_category_3, wiki_category_4, reg_category):
+
+    insert_into_videos = "INSERT INTO popular_chart (run_date, day, video_title, video_id, channel_title, channel_id, published_at, tags, category_id, view_count, like_count, dislike_count, favorite_count, comment_count, for_kids, wiki_category_1, wiki_category_2, wiki_category_3, wiki_category_4, reg_category) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
+    row_to_insert = (run_date, day, video_title, video_id, channel_title, channel_id, published_at, tags, category_id, view_count, like_count, dislike_count, favorite_count, comment_count, for_kids, wiki_category_1, wiki_category_2, wiki_category_3, wiki_category_4, reg_category)
+    try:
+        curr.execute(insert_into_videos, row_to_insert)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+def append_db(curr, conn, df):
+
+    try:
+        for i, row in df.iterrows():
+            insert_table(curr, row['run_date'], row['day'], row['video_title'], row['video_id'], row['channel_title'],
+                         row['channel_id'], row['published_at'], row['tags'], row['category_id'], row['view_count'],
+                         row['like_count'], row['dislike_count'], row['favorite_count'], row['comment_count'], row['for_kids'],
+                         row['wiki_category_1'], row['wiki_category_2'], row['wiki_category_3'], row['wiki_category_4'], row['reg_category'])
+            conn.commit()
+        curr.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def postgresql_run():
 
     starttime = datetime.now()
     print(starttime)
@@ -160,24 +209,23 @@ def chart_export(key):
     cols = cols[-2:] + cols[:-2]
     youtube_popular = youtube_popular[cols]
 
-    if key == 'new':
-        # CASE 1 : Push DF to Table
-        params = S3Connection(os.environ['SQL_URL'])
-        engine = create_engine(params)
-        youtube_popular.to_sql('popular_chart', engine, index=False)
+    # Connect to Postgresql
+    host = os.environ['SQL_HOST']
+    database = os.environ['SQL_DB']
+    user = os.environ['SQL_USER']
+    port = os.environ['SQL_PORT']
+    password = os.environ['SQL_PW']
 
-    elif key == 'update':
-        # CASE 2 : Append DF to Table
-        params = S3Connection(os.environ['SQL_URL'])
-        engine = create_engine(params)
-        youtube_popular.to_sql('popular_chart', engine, if_exists='append', index=False)
+    params_dic = config(host=host, database=database, user=user, port=port, password=password)
+    conn = connect(params_dic)
+    curr = conn.cursor()
+
+    # Inserting each row
+    append_db(curr=curr, conn=conn, df=youtube_popular)
 
     endtime = datetime.now()
     print(endtime)
     timetaken = endtime - starttime
     print('Time taken : ' + timetaken.__str__())
 
-def test_app():
-    chart_export(key='update')
-
-test_app()
+postgresql_run()
